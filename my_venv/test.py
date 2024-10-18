bbdf1097edfb39146eb67b17bd912826a49c04fb
Hackedbug <ahmedyinusa2004@gmail.com>
2024-10-18 16:05:40 +0100

    Initial commit

---
+++ my_venv/test.py
@@ -0,0 +1,5 @@
+print("Hello World. This is Ahmed sending this code from vscode linked to his wsl distro.")
+input("Enter your full name: ")
+float(input("Enter your age: "))
+
+print("Welcome aboard the A-train.")
