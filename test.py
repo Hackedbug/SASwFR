b0142935099f379b63d98f3bd430cced6c2b7401
Hackedbug <ahmedyinusa2004@gmail.com>
2024-10-20 00:20:32 +0100

    Added a function for basic calc. with lists and while loop

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
-#return statement used to return certain values in the program
-def cube(num):
-	return num*num*num
+import math
+def calc(num1, op, num2): #created a function for basic calculator
+    operators = ['+', '-', '*', '/', '%', 'sqrt', 'sqr'] #list of the known operators used in maths on the calculator
 
-result = cube(float(input("Enter the number you want to be cubed: ")))
-print(result)
+    while op in operators:
+        if op == "+": #addition
+            return num1 + num2
+        elif op == "-": #subtraction
+            return num1 - num2
+        elif op == "*": #multiplication
+            return num1 * num2
+        elif op == "/": #divison
+            return num1 / num2
+        elif op == "%": #modulus
+            return num1 % num2
+        elif op == "sqrt":
+            return math.sqrt(num1)
+        else:
+            return "Nothing."
+    else: #for if the "operator" provided by the user is not among the list
+        return "Error occured in the calculation. " + op + " is an Invalid Operator"
+
+num1 = float(input("Enter the first number: ")) #for the user to input the first number
+op = input("Enter the operator: ") #for the user to input the operator
+num2 = float(input("Enter the second number: ")) #for the user to input the second number
+
+print(calc(num1, op, num2))
